395f21e43d7473cd8855ffcbb90e0a58f4deb476
Andre Moura <andremarquesmoura@gmail.com>
2019-01-18 14:32:28 -0800

    Add smooth scrolling and Flickity carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
   console.log("Yup! We're connected!");
+
+  /* Smooth Scrolling
+     @source https://css-tricks.com/smooth-scrolling-accessibility/ */
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
+  // End of Smooth Scrolling
+
+  $(".main-carousel").flickity({
+    percentPosition: false,
+    imagesLoaded: true,
+    autoPlay: true,
+    prevNextButtons: false,
+    cellAlign: "left",
+    contain: true
+  });
 });
